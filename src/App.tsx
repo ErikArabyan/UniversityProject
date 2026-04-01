615294e0df8388c98e5b59dd6186fdc3f5040081
Erik <erik.arabyan@mail.ru>
2026-04-01 17:23:48 +0400

    optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Switch, Route, Router as WouterRouter } from 'wouter';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/toaster';
-import { TooltipProvider } from '@/components/ui/tooltip';
 import { Layout } from '@/components/layout';
 
 import Home from '@/pages/home';
 import LibraryOverview from '@/pages/library-overview';
 import Lesson from '@/pages/lesson';
 import NotFound from '@/pages/not-found';
-
-const queryClient = new QueryClient();
 
 function Router() {
 	return (
@@ -26,14 +21,9 @@ function Router() {
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<TooltipProvider>
-				<WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, '')}>
-					<Router />
-				</WouterRouter>
-				<Toaster />
-			</TooltipProvider>
-		</QueryClientProvider>
+		<WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+			<Router />
+		</WouterRouter>
 	);
 }
 
